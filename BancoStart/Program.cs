78a27bc312d7af21d9aa892572dd718d8f6b4d1e
carlos.eduardo <carlos.eduardo@TDS-SUP66>
2023-06-27 15:52:58 -0300

    valor errado

--- BancoStart/Program.cs
+++ BancoStart/Program.cs
@@ -11,16 +11,16 @@
 
             int Inicio;
 
-            if (int.TryParse(Console.ReadLine(), out Inicio))
+            if (int.TryParse(Console.ReadLine(), out Inicio)) // tenta converter o que foi inserido em um valor numérico
             {
-                if (Inicio == 1 || Inicio == 2 || Inicio == 0)
+                if (Inicio == 1 || Inicio == 2 || Inicio == 0) // valida se o valor inserido é um das 3 opções 
                 {
                     if (Inicio == 1)
                     {
                         Console.WriteLine("realizar saque");
                         Thread.Sleep(150);
                     }
-                    else if (Inicio == 2)
+                    else if (Inicio == 2) 
                     {
                         Console.WriteLine("verificar Saldo");
                         Thread.Sleep(150);
@@ -28,7 +28,7 @@
                     }
                     Console.Clear();
                 }
-                else
+                else //caso o numero inserido nao seja as opções
                 {
                     Console.Clear();
                     Console.WriteLine("código invalido inserido!\n\nAperte qualquer tecla para voltar");
@@ -37,7 +37,7 @@
                     return InicioM();
                 }
             }
-            else
+            else // caso o que foi inserido nao for um numero
             {
                 Console.Clear();
                 Console.WriteLine("Entrada invalida!!Por favor insira um valor numérico\n\nAperte qualquer tecla para voltar");
@@ -51,9 +51,9 @@
 
         static void Main()
         {
-            int q200 = 100, q100 = 300, q50 = 500, q25 = 600, q10 = 700, q05 = 800, q02 = 1000, Cedulas = 0, Saldo1 = 600, Saldo2; //variaveis  
-            int escolha = InicioM();
-            if (escolha == 2)
+            int q200 = 100, q100 = 300, q50 = 500, q20 = 600, q10 = 700, q05 = 800, q02 = 1000, Cedulas = 0, Saldo1 = 600, Saldo2; //variaveis  
+            int escolha = InicioM();// receber o que foi retornado no metodo anterior em uma variavel
+            if (escolha == 2) //o que fazer ao escolher as opções do menu
             {
                 Console.Clear();
                 Console.WriteLine($"O seu saldo e de R$ {Saldo1}");
@@ -62,27 +62,25 @@
                 Console.Clear();
             }
 
-            else if (escolha == 0)
+            else if (escolha == 0)//o que fazer ao escolher as opções do menu
             {
                 Console.WriteLine("Saindo do sistema...");
                 Environment.Exit(0);
             }
 
-            //para baixo esta certo, mas o menu nao funciona
             Console.WriteLine("cedulas disponiveis\n");
             Console.WriteLine(" R$ 200,00\t" + q200);
             Console.WriteLine("\n R$ 100,00\t" + q100);
             Console.WriteLine("\n R$ 50,00\t" + q50);
-            Console.WriteLine("\n R$ 25,00\t" + q25);
+            Console.WriteLine("\n R$ 20,00\t" + q20);
             Console.WriteLine("\n R$ 10,00\t" + q10);
             Console.WriteLine("\n R$ 5,00\t" + q05);
             Console.WriteLine("\n R$ 2,00\t" + q02);
             Console.WriteLine("\n\n Aperte qualquer tecla para continuar");
             Console.ReadKey();
             Console.Clear();
-
-            //Boolean start = true;
-            Console.WriteLine("Selecione o valor que deseja sacar: ");
+            //opções de cedulas disponiveis na maquina
+            Console.WriteLine("Selecione o valor que deseja sacar: "); //receber quanto quer sacar
             int SaqueI;// valor de saque
             if (int.TryParse(Console.ReadLine(), out SaqueI))
             {
@@ -91,7 +89,7 @@
 
                 if (Saque < Saldo1)  // faz a verificação do saldo com o valor a ser sacado
                 { 
-                    if (Saque %2 == 0)
+                    if (Saque %2 == 0)//verifica se o saque é divisivel por 2 e faz a leitura do resto
                     {
                         Console.WriteLine("O saque será de: R$ " + Saque);
                         while (Saque != 0)
@@ -121,6 +119,13 @@
                                     Saque -= 50;
                                     Cedulas++;
                                     break;
+                                case >= 20:
+                                    q20--;
+                                    Console.WriteLine("\nCedula de R$ 25 sacada");
+                                    Thread.Sleep(1500);
+                                    Saque -= 20;
+                                    Cedulas++;
+                                    break;
                                 case >= 10:
                                     q10--;
                                     Console.WriteLine("\nCedula de R$ 10 sacada");
@@ -141,7 +146,7 @@
                         }
 
                     }
-                    else
+                    else // se nao for divisivel por 2 vem para esse segundo looping
                     {
                         while (Saque != 0)
                         {
@@ -166,13 +171,6 @@
                                     Console.WriteLine("\nCedula de R$ 50 sacada");
                                     Thread.Sleep(1500);
                                     Saque -= 50;
-                                    Cedulas++;
-                                    break;
-                                case >= 25:
-                                    q25--;
-                                    Console.WriteLine("\nCedula de R$ 25 sacada");
-                                    Thread.Sleep(1500);
-                                    Saque -= 25;
                                     Cedulas++;
                                     break;
                                 case >= 10:
@@ -200,21 +198,21 @@
                         }
                             }
                     Console.Clear();
-                    Saldo2 = Saldo1 - SaqueI;
-                    Console.WriteLine($" A quantidade de Cedulas sacadas para o valor R$ {SaqueI} foi de: {Cedulas} cedulas\n\t E agora seu saldo ficou em {Saldo2}!"); //resposta caso haja o saque
+                    Saldo2 = Saldo1 - SaqueI;//saber quanto foi o saldo que restou para o cliente
+                    Console.WriteLine($" A quantidade de Cedulas sacadas para o valor R$ {SaqueI} foi de: {Cedulas} cedulas\n\n E agora seu saldo ficou em {Saldo2}!"); //resposta caso haja o saque
                 }
 
 
                 else
                 {
-                    Console.WriteLine($"Saldo indisponivel seu saldo e de {Saldo1}"); // resposta para pergunta se quer realizar
+                    Console.WriteLine($"Saldo indisponivel seu saldo e de {Saldo1}"); // resposta caso o saldo for menor que a quantia desejada no saque
                 }
 
             }
             else
             {
                 Console.Clear();
-                Console.WriteLine("Informação invalida por favor insira um valor que deseja sacar Ex: 500");
+                Console.WriteLine("Informação invalida por favor insira um valor que deseja sacar Ex: 500");//resposta caso o valor inserido for diferente de inteiro
             }
         }
     }
